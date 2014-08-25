64a583887ed1fb9dfb8401c1d24fd1ba844caf2d
Miguel Sancho <sanchocuba@gmail.com>
2014-08-24 22:19:58 -0400

    fixed dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
+
 //= require chartkick
 
 //= require_tree .
