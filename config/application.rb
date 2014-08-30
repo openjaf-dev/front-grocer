20315b44311f7c6b9d80155514eca48d8a270a45
Miguel Sancho <sanchocuba@gmail.com>
2014-08-30 00:17:38 -0400

    assets precompile

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,6 @@ module Grocer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.compile = true
 
   end
 end
