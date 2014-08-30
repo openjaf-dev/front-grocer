04331916d870aacd3e1e02a58397e554a6c7817a
Miguel Sancho <sanchocuba@gmail.com>
2014-08-30 00:08:58 -0400

    update assets precompile

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module Grocer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.compile = true
+
   end
 end
