32c8a16f71d89d81c88246b949b8fa6cc5c52bf7
Miguel Sancho <sanchocuba@gmail.com>
2014-09-01 17:57:58 -0400

    add fonts

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Grocer
     end
     
     config.autoload_paths += %W(#{config.root}/lib)
+    
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
